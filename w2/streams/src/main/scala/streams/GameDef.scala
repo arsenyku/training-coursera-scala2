4eeaf783d741d27d95062b9a1d165384522ff360
asu <arsenyk.ustaris.dev@gmail.com>
2017-04-09 15:24:04 -0700

    neighbors and legalNeighbors

--- w2/streams/src/main/scala/streams/GameDef.scala
+++ w2/streams/src/main/scala/streams/GameDef.scala
@@ -132,13 +132,21 @@ trait GameDef {
      * Returns the list of blocks that can be obtained by moving
      * the current block, together with the corresponding move.
      */
-    def neighbors: List[(Block, Move)] = ???
+    def neighbors: List[(Block, Move)] =
+    {
+      val blocks = left :: right :: up :: down :: Nil
+      val moves = Left :: Right :: Up :: Down :: Nil
+      blocks zip moves
+    }
 
     /**
      * Returns the list of positions reachable from the current block
      * which are inside the terrain.
      */
-    def legalNeighbors: List[(Block, Move)] = ???
+    def legalNeighbors: List[(Block, Move)] =
+    {
+      neighbors.filter({ case (b,_) => terrain(b.b1) && terrain(b.b2) })
+    }
 
     /**
      * Returns `true` if the block is standing.
