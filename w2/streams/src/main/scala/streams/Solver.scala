7bed6d2890121161211c35a5132ea54b080ffa9c
asu <arsenyk.ustaris.dev@gmail.com>
2017-04-10 02:33:51 -0700

    Implements from, pathsFromStart, pathsToGoal, solution.  Passes 9/10

--- w2/streams/src/main/scala/streams/Solver.scala
+++ w2/streams/src/main/scala/streams/Solver.scala
@@ -75,18 +75,35 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] =
+  {
+    lazy val neighbors = (for
+    {
+      (block, moves) <- initial
+      newNeighbors = newNeighborsOnly(neighborsWithHistory(block, moves), explored)
+      if newNeighbors.nonEmpty
+    } yield newNeighbors).flatten
+
+    lazy val newExplored = neighbors.map({ case (b,_) => b })
+
+    initial #::: from(neighbors, explored union newExplored.toSet)
+  }
 
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = from(Stream((startBlock, List())), Set(startBlock))
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] =
+  {
+    pathsFromStart
+      .map({ case(block,moveList) => (block,moveList.reverse) })
+      .filter({ case(block, _) => done(block) })
+  }
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -96,5 +113,5 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = pathsToGoal.head._2
 }
