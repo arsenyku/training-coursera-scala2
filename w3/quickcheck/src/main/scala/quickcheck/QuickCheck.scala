3e39758b8cb013d18b569f5055aca428949e210b
asu <arsenyk.ustaris.dev@gmail.com>
2017-04-16 14:19:36 -0700

    Tests for empty Heap handling.  Passes all w3 tests.

--- w3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ w3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -26,4 +26,37 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
+  // If you insert any two elements into an empty heap, finding the minimum of the resulting heap
+  // should get the smallest of the two elements back.
+  property("minOf2") =
+  {
+    val m = 749
+    val expectedMin = 92
+    val twoElementHeap = insert(expectedMin, insert(m, self.empty))
+    findMin(twoElementHeap) == expectedMin
+  }
+
+  property("correctMin") = forAll
+  { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    val expectedMin = m - 1
+    findMin(insert(expectedMin, h)) == expectedMin
+  }
+
+
+  // If you insert an element into an empty heap, then delete the minimum, the resulting heap should
+  // be empty.
+  property("deleteLast") = forAll
+  { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    val expectedMin = m - 1
+    findMin(insert(expectedMin, h)) == expectedMin
+  }
+
+  // Given any heap, you should get a sorted sequence of elements when continually finding and
+  // deleting minima. (Hint: recursion and helper functions are your friends.)
+
+  // Finding a minimum of the melding of any two heaps should return a minimum of one or the other.
+
+
 }
