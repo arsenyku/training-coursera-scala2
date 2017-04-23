436a09652fed9880e2b4991603ac1556f71d1048
asu <arsenyk.ustaris.dev@gmail.com>
2017-04-23 11:32:18 -0700

    Implements roots solver.

--- w4/calculator/src/main/scala/calculator/Polynomial.scala
+++ w4/calculator/src/main/scala/calculator/Polynomial.scala
@@ -2,12 +2,26 @@ package calculator
 
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+      c: Signal[Double]): Signal[Double] =
+  {
+    Signal( (b()*b()) - (4*a()*c()) )
+    // (b*b) + (4*a*c)
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    // (-b +- sqrt(delta))/2a
+    Signal(computeRoots(a(),b(),c(),delta()))
+  }
+
+  def computeRoots(a: Double, b: Double, c:Double, delta: Double): Set[Double] =
+  {
+    if ( delta < 0 ) Set()
+    else
+    {
+      val root1 = (-b + Math.sqrt(delta)) / (2 * a)
+      val root2 = (-b - Math.sqrt(delta)) / (2 * a)
+      Set(root1, root2)
+    }
   }
 }
